ff8f9277d81d4915bde1f3234f3ad95b91a12097
Теори Накубуру <nakuburu@yandex.ru>
2021-01-30 20:03:21 +0200

    Добавьте файлы проекта.

---
+++ Nf,ekbhjdfybt aeyrwbq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nf_ekbhjdfybt_aeyrwbq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y;
+            double pi = Math.PI;
+            for (double x = 0.1; x == 2.5; x += 0.2)
+            {
+                y = Math.Pow(x, 2) + 2 * pi * Math.Cos(pi * x);
+                Console.WriteLine(y);
+            }
+            Console.ReadKey();
+        }
+    }
+}
